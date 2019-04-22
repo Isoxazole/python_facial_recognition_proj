ffb6e0c5847a8ddb2b26cce51aaa19109951356e
riley <rjfitzg@outlook.com>
2019-04-22 21:24:47 +0000

    Working face rec

--- faceRec.py
+++ faceRec.py
@@ -7,19 +7,24 @@ with open("img_paths.txt") as f:
 file_locs = [x.strip() for x in file_locs]
 
 # Load known face to test off of. Working from lfw dataset.
-known_face = fr.load_image_file("/root/Data/known/Tim_Allen_0001.jpg")
-known_face_enc = fr.face_encodings(known_face)[0] # This can error out if no face is in image
-
-# Create variable for file location, this should not be needed in final implementation
-work_dir = "/root/Data/unknown/"
+try:
+	known_face = fr.load_image_file("/root/PythonFinalProj/known/Tim_Allen_0001.jpg")
+	known_face_enc = fr.face_encodings(known_face)[0] 
+except:
+	#import os; os.system('exit')
+	print('There was an error loading your known face')
 
 # Here is the code to make this project work.
 # It needs to undergo a for loop for every known image we have.
 # https://github.com/ageitgey/face_recognition/blob/master/examples/recognize_faces_in_pictures.py
 for file_loc in file_locs:
-	print(file_loc)
-	curr_test_img = work_dir + file_loc
-	unk_face = fr.load_image_file(curr_test_img)
-	unk_face_enc = fr.face_encodings(unk_face)[0]
-	result = fr.compare_faces([known_face_enc], unk_face_enc)
-	print(result)
+	try:
+		unk_face = fr.load_image_file(file_loc)
+		unk_face_enc = fr.face_encodings(unk_face)[0]
+		result = fr.compare_faces([known_face_enc], unk_face_enc)
+	except:
+		# No face was found in file.
+		# Or error loading file..
+		print('NO FACES WERE FOUND HERE')
+	if (result == [True]):
+		print("MATCH: %s" % file_loc)
